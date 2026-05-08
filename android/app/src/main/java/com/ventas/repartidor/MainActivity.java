9d1e027a7f9dffa217333d0e531e28e98b65ce6a
Developer <developer@example.com>
2026-05-07 18:09:56 -0700

    debug: enable WebView remote debugging via chrome://inspect

--- android/app/src/main/java/com/ventas/repartidor/MainActivity.java
+++ android/app/src/main/java/com/ventas/repartidor/MainActivity.java
@@ -1,5 +1,15 @@
 package com.ventas.repartidor;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Habilitar Chrome Remote Debugging para poder inspeccionar
+        // la app desde chrome://inspect en la PC
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
